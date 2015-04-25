d1cc1fc5b823dede5078502ff296698e18495578
Oliver Poole <oliverpoole7@gmail.com>
2015-04-25 10:33:55 +0100

    Initial project setup

---
+++ BattleHack2015/BattleHack2015/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BattleHack2015
+//
+//  Created by Oliver Poole on 25/04/2015.
+//  Copyright (c) 2015 Team George & Ollie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BattleHack2015/BattleHack2015/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BattleHack2015
+//
+//  Created by Oliver Poole on 25/04/2015.
+//  Copyright (c) 2015 Team George & Ollie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
